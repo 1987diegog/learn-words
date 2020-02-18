cbfe4bbc373feef5fcfad4bcff18a9b2d9826ef4
DiegoG09 <1987diegog@gmail.com>
2020-02-18 12:36:42 -0300

    First Commit

---
+++ src/main/java/com/demente/ideas/learnwords/model/RoleName.java
@@ -0,0 +1,6 @@
+package com.demente.ideas.learnwords.model;
+
+public enum RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/test/java/com/demente/ideas/learnwords/LearnWordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demente.ideas.learnwords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnWordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
